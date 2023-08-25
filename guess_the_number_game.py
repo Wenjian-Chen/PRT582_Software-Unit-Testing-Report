ef3f822494884e8d242179d8c665fc1a86f1b7ba
Wenjian-Chen <wjchen1001@gmail.com>
2023-08-25 22:44:12 +0930

    Submit Assignment

--- guess_the_number_game.py
+++ guess_the_number_game.py
@@ -1,20 +1,22 @@
 """This module is a simple Guess the Number game where the player needs to"""
 import random
+import sys
 
 
 class GuessTheNumberGame:
     """
     A simple Guess the Number game where the player needs to\
         guess a randomly generated 4-digit number.
-    
+
     The game provides hints to the player \
         about the accuracy of their guess using 'circle' and 'x' symbols.
     'circle' indicates that one digit is correct and in the right spot,\
         while 'x' indicates a correct digit in the wrong spot.
-    
+
     Attributes:
         generated_number (str): The randomly generated 4-digit number.
-        attempts (int): The number of attempts taken to guess the correct number.
+        attempts (int):\
+            The number of attempts taken to guess the correct number.
     """
 
     def __init__(self):
@@ -30,6 +32,7 @@ class GuessTheNumberGame:
             and assigns it to the `generated_number` attribute of the class.
         """
         self.generated_number = str(random.randint(1000, 9999))
+
     def compare_numbers(self, guess):
         """
         Compare the user's guess with the generated number and provide hints.
@@ -38,24 +41,25 @@ class GuessTheNumberGame:
             guess (str): The user's guess (4-digit number).
 
         Returns:
-            The number of correct digits in the correct spot (circle_count)\
-                and the number of correct digits in the wrong spot (x_count).
+            str: hints consist of 'O' and 'X' \
+                indicating correct and incorrect digits respectively.
         """
-        # Initialize counts
-        circle_count = 0  # Count of correct digits in the correct spot
-        x_count = 0  # Count of correct digits in the wrong spot
+        # Initialize hints
+        hints = ""
 
         # Iterate over each digit in the guess
         for i in range(4):
             # Check if the digit is in the correct spot
             if guess[i] == self.generated_number[i]:
-                circle_count += 1
+                hints += "O"
             # Check if the digit is in the wrong spot
             elif guess[i] in self.generated_number:
-                x_count += 1
+                hints += "X"
+            else:
+                hints += "_"
 
-        # Return the counts
-        return circle_count, x_count
+        # Return the hints
+        return hints
 
     def play(self):
         """Play the Guess the Number game."""
@@ -65,7 +69,7 @@ class GuessTheNumberGame:
             guess = input("Enter your guess (4 digits) or 'q' to quit: ")
             # Check if user wants to quit
             if guess.lower() == "q":
-                break
+                sys.exit()
 
             # Check if input is valid
             if len(guess) != 4 or not guess.isdigit() or int(guess) < 1000:
@@ -77,28 +81,26 @@ class GuessTheNumberGame:
 
             # Update attempts count
             self.attempts += 1
-
             # Compare user's guess with the generated number
-            circle_count, x_count = self.compare_numbers(guess)
+            hints = self.compare_numbers(guess)
 
             # Check if user guessed the number
-            if circle_count == 4:
+            if hints == "OOOO":
                 print(
                     f"Congratulations! \
                         You guessed the number in {self.attempts} attempts."
                 )
-                replay = input("Do you want to play again? (y/n): ")
 
+                replay = input("Do you want to play again? (y/n): ")
                 # Check if user wants to play again
                 if replay.lower() == "y":
                     self.attempts = 0
                     self.generate_number()  # Generate a new random number
                 else:
-                    print("Thanks for playing!")
-                    break  # Quit the game
+                    sys.exit()  # Quit the game
             else:
                 # Print hints
-                print(f"Hints: {circle_count} circles, {x_count} x's")
+                print(f"hints: {hints}")
 
 
 if __name__ == "__main__":
--- test_guess_the_number_game.py
+++ test_guess_the_number_game.py
@@ -12,25 +12,25 @@ class MyTestCase(unittest.TestCase):
     def test_generate_number(self):
         self.results.generate_number()
         self.assertIsNotNone(self.results.generated_number)
-        self.assertTrue(1000 <= int(self.results.generated_number) <= 9999)
+        self.assertTrue(
+            1000 <= int(self.results.generated_number) <= 9999
+            and len(self.results.generated_number) == 4
+            and self.results.generated_number.isdigit()
+        )
 
     def test_compare_numbers(self):
         self.results.generated_number = "1234"
-        circle_count, x_count = self.results.compare_numbers("1234")
-        self.assertEqual(circle_count, 4)
-        self.assertEqual(x_count, 0)
+        Hints = self.results.compare_numbers("1234")
+        self.assertEqual(Hints, "OOOO")
 
-        circle_count, x_count = self.results.compare_numbers("5678")
-        self.assertEqual(circle_count, 0)
-        self.assertEqual(x_count, 0)
+        Hints = self.results.compare_numbers("5678")
+        self.assertEqual(Hints, "____")
 
-        circle_count, x_count = self.results.compare_numbers("1342")
-        self.assertEqual(circle_count, 1)
-        self.assertEqual(x_count, 3)
+        Hints = self.results.compare_numbers("1342")
+        self.assertEqual(Hints, "OXXX")
 
-        circle_count, x_count = self.results.compare_numbers("1243")
-        self.assertEqual(circle_count, 2)
-        self.assertEqual(x_count, 2)
+        Hints = self.results.compare_numbers("1243")
+        self.assertEqual(Hints, "OOXX")
 
     @patch("random.randint", return_value=1234)
     @patch("builtins.input", side_effect=["abcd", "123", "1235", "1234", "q"])
@@ -39,22 +39,30 @@ class MyTestCase(unittest.TestCase):
         Check if the play function run successfully, and the outputs and attempts are correct.
         """
         with patch("sys.stdout", new=StringIO()) as captured_output:
-            self.results.generated_number = "1234"
+            with self.assertRaises(SystemExit):
+                self.results.play()
+                captured_output = captured_output.getvalue().strip().split("\n")
+                expected_output1 = "Invalid input. \
+                            Please enter a 4-digit number between 1000 and 9999."
+                expected_output2 = "Invalid input. \
+                            Please enter a 4-digit number between 1000 and 9999."
+                expected_output3 = "Hints: OOO_"
+                expected_output4 = "Congratulations! \
+                            You guessed the number in 2 attempts."
+                self.assertEqual(captured_output[-1], expected_output4)
+                self.assertEqual(captured_output[-2], expected_output3)
+                self.assertEqual(captured_output[-3], expected_output2)
+                self.assertEqual(captured_output[-4], expected_output1)
+                self.assertEqual(self.results.attempts, 2)
+
+    @patch("random.randint", return_value=1234)
+    @patch("builtins.input", side_effect=["1234", "y", "2143", "1234", "q"])
+    def test_play_replay(self, _, __):
+        """
+        Check if replay successfully by check the attempts
+        """
+        with self.assertRaises(SystemExit):
             self.results.play()
-            captured_output = captured_output.getvalue().strip().split("\n")
-            expected_output1 = "Invalid input. \
-                        Please enter a 4-digit number between 1000 and 9999."
-            expected_output2 = "Invalid input. \
-                        Please enter a 4-digit number between 1000 and 9999."
-            expected_output3 = "Hints: 3 circles, 0 x's"
-            expected_output4 = "Congratulations! \
-                        You guessed the number in 2 attempts."
-            expected_output5 = "Thanks for playing!"
-            self.assertEqual(captured_output[-1], expected_output5)
-            self.assertEqual(captured_output[-2], expected_output4)
-            self.assertEqual(captured_output[-3], expected_output3)
-            self.assertEqual(captured_output[-4], expected_output2)
-            self.assertEqual(captured_output[-5], expected_output1)
             self.assertEqual(self.results.attempts, 2)
 
 
